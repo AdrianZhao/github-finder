0fd5425bcd16bdbf7a30028570cc1356f3b185c8
Yuhan Zhao <achnnal@gmail.com>
2023-05-20 19:00:50 -0500

    bug fix

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,33 +1,48 @@
-
-import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate();
+  const token = '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';
+  const options = {headers: { Authorization: `Bearer ${token}`}};
 
-  const { register, handleSubmit, formState } = useForm();
-  const { errors } = formState;
-  const history = useNavigate();
-  const onSubmit = ({ username }) => {
-    history(`/user/${username}`);
+  const isUser = async (name) => {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${name}`, options);
+      console.log(response.data);
+      navigate(`/user/${name}`);
+    } catch (error) {
+      setErrorMessage('User not found. Please try again');
+      name = '';
+    }
+  };
+  const onSubmit = ({ name }) => {
+    isUser(name);
   };
 
   return (
-    <div className="wrapper">
-      <h2>Github Finder</h2>
-      <form className="search-wrapper" onSubmit={handleSubmit(onSubmit)}>
-        <input 
-          type='text' 
-          {...register('name', { 
-            required: { 
-              value: true, 
-              message: "Name is reuqired"
-            }
-          })} 
-        />
-      <input type="submit"></input>
-      </form>
-      <p className="message">{errors.name?.message}</p>
+    <div className='container box'>
+      <div className="wrapper">
+        <h2>Github Finder</h2>
+        <p className="message"><br />{errors.name?.message}</p>
+        <form className="search-wrapper" onSubmit={handleSubmit(onSubmit)}>
+          <input 
+            type='text' 
+            {...register('name', { 
+              required: { 
+                value: true, 
+                message: "Name is reuqired",
+              }
+            })} 
+          />
+        <input type="submit"></input>
+        </form>
+        <p className="message">{errorMessage}</p>
+      </div>
     </div>
   );
 };
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,36 +1,71 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
+import { Link } from 'react-router-dom'
 const User = () => {
   const { username } = useParams();
-  const [user, setUser] = useState(null);
-
+  const [user, setUser] = useState('');
+  const [repos, setRepos] = useState([]);
   useEffect(() => {
+    const token = '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';
+    const options = {headers: { Authorization: `Bearer ${token}`}};
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+        const response = await axios.get(`https://api.github.com/users/${username}`, options);
         setUser(response.data);
       } catch (error) {
-        console.error(error);
+        setUser('');
       }
     };
     fetchUser();
+    const fetchRepo = async () => {
+      try {
+        const response = await axios.get(`https://api.github.com/users/${username}/repos`, options);
+        setRepos(response.data);
+      } catch (error) {
+        setRepos([]);
+      }
+    };
+    fetchRepo();
   }, [username]);
-
-  if (!user) {
-    return <div>User not found.</div>;
-  }
-
   return (
-    <div>
-      <p>Username: {user.login}</p>
-      <p>Followers: {user.followers}</p>
-      <p>Following: {user.following}</p>
-      <p>Public Repositories: {user.public_repos}</p>
-      <p>Location: {user.location}</p>
-    </div>
-  );
+    <section>
+      <div className='container board'>
+        <div className='info-wrapper'>
+          <img src={user.avatar_url} alt={user.name} />
+          <h1>{user.login}</h1>
+          <div className='detail-wrapper'>
+            <p>Repositories:<span>{user.public_repos}</span></p>
+            <p>Followers:<span>{user.followers}</span></p>
+            <p>Following:<span>{user.following}</span></p>
+          </div>
+          <button>
+            <Link 
+              className='link' 
+              target="_blank"
+              to={`https://github.com/${user.login}`}>
+              GitHub Page
+            </Link>
+          </button>
+        </div>
+        <div className='repos-wrapper'>
+          <h2>My Repositories</h2>
+          {repos.sort((a, b) => b.id - a.id).map((repo) => (
+            <div className="repo" key={repo.id}>
+              <div>
+                <h3>{repo.name}</h3>
+                <p>{repo.description}</p>
+              </div>
+              <div className="time">
+                Updated on {new Date(repo.updated_at).toLocaleDateString('en-US', 
+                { year: 'numeric', month: 'long', day: 'numeric' })}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default User;
